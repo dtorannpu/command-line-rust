fadc087329696152a9e8e2c3a506c0da213deb61
dtorannpu <dtorannpu@gmail.com>
2024-02-04 16:38:26 +0900

    第３章終了

---
+++ 03_catr/src/lib.rs
@@ -0,0 +1,91 @@
+use std::error::Error;
+use std::fs::File;
+use std::io;
+use std::io::{BufRead, BufReader};
+use clap::{Arg, ArgAction, Command};
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    number_lines: bool,
+    number_nonblank_lines: bool,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = Command::new("catr")
+        .version("0.1.0")
+        .author("dtorannpu")
+        .about("Rust cat")
+        .arg(
+            Arg::new("files")
+                .value_name("FILE")
+                .help("Input file(s)")
+                .required(true)
+                .action(ArgAction::Append)
+                .default_value("-")
+        )
+        .arg(
+            Arg::new("number")
+                .short('n')
+                .long("number")
+                .action(ArgAction::SetTrue)
+                .help("Number lines")
+                .conflicts_with("number_nonblank")
+        )
+        .arg(
+            Arg::new("number_nonblank")
+                .short('b')
+                .long("number-nonblank")
+                .action(ArgAction::SetTrue)
+                .help("Number non-blank lines")
+        )
+        .get_matches();
+
+    let files = matches.get_many::<String>("files")
+        .expect("files required")
+        .map(|v| v.to_string())
+        .collect::<Vec<_>>();
+
+    Ok(Config {
+        files,
+        number_lines: matches.get_flag("number"),
+        number_nonblank_lines: matches.get_flag("number_nonblank"),
+    })
+}
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+pub fn run(config: Config) -> MyResult<()> {
+    for filename in config.files {
+        match open(&filename) {
+            Err(err) => eprintln!("Failed to open {}: {}", filename, err),
+            Ok(file) => {
+                let mut last_num = 0;
+                for (line_num, line_result) in file.lines().enumerate() {
+                    let line = line_result?;
+
+                    if config.number_lines {
+                        println!("{:>6}\t{}", line_num + 1, line);
+                    } else if config.number_nonblank_lines {
+                        if !line.is_empty() {
+                            last_num += 1;
+                            println!("{:>6}\t{}", last_num, line);
+                        } else {
+                            println!();
+                        }
+                    } else {
+                        println!("{}", line);
+                    }
+                }
+            }
+        }
+    }
+    Ok(())
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
