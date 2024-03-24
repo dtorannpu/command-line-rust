fc06fe65a40f09fc46b4288007c0bf1f1491e244
dtorannpu <dtorannpu@gmail.com>
2024-03-24 12:06:51 +0900

    uniqr実装

---
+++ 06_uniqr/src/lib.rs
@@ -0,0 +1,100 @@
+use std::error::Error;
+use std::fs::File;
+use std::io;
+use std::io::{BufRead, BufReader, Write};
+
+use clap::{Arg, Command};
+use clap::ArgAction::{Set, SetTrue};
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    in_file: String,
+    out_file: Option<String>,
+    count: bool,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = Command::new("uniq")
+        .version("0.1.0")
+        .about("Rust uniq")
+        .arg(Arg::new("in_file")
+            .value_name("IN_FILE")
+            .help("Input file [default: -]")
+            .default_value("-")
+            .action(Set)
+        ).
+
+        arg(Arg::new("out_file")
+            .value_name("OUT_FILE")
+            .help("Output file")
+        )
+        .arg(Arg::new("count")
+            .value_name("count")
+            .short('c')
+            .long("count")
+            .help("Show counts")
+            .action(SetTrue)
+        )
+
+        .get_matches();
+
+    Ok(Config {
+        in_file: matches.get_one::<String>("in_file").unwrap().to_string(),
+        out_file: matches.get_one::<String>("out_file").cloned(),
+        count: matches.get_flag("count"),
+    })
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    let mut file = open(&config.in_file)
+        .map_err(|e| format!("{}: {}", config.in_file, e))?;
+
+    let mut out_file: Box<dyn Write> = match &config.out_file {
+        Some(out_name) => Box::new(File::create(out_name)?),
+        _ => Box::new(io::stdout()),
+    };
+
+    let mut line = String::new();
+    let mut previous = String::new();
+    let mut count: u64 = 0;
+
+    let mut print = |count: u64, text: &str| -> MyResult<()> {
+        if count > 0 {
+            if config.count {
+                write!(out_file, "{:>4} {}", count, text)?;
+            } else {
+                write!(out_file, "{}", text)?;
+            }
+        }
+        Ok(())
+    };
+
+    loop {
+        let bytes = file.read_line(&mut line)?;
+        if bytes == 0 {
+            break;
+        }
+
+        if line.trim_end() != previous.trim_end() {
+            print(count, &previous)?;
+            previous = line.clone();
+            count = 0;
+        }
+
+        count += 1;
+        line.clear();
+    }
+
+    print(count, &previous)?;
+
+    Ok(())
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
